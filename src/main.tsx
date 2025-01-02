b900d3d516a6f9e192ebca50b5bc64275dc60667
zacharie <zacharie@tshoko.fr>
2025-01-02 19:42:46 +0100

    fix app import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import App from './app.tsx';
 import './assets/globals.css';
 
 createRoot(document.getElementById('root')!).render(
