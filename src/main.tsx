94989d97815f7b873a30edd34b445cb04991337e
zacharie <zacharie@tshoko.fr>
2025-01-02 19:48:41 +0100

    remove extension from app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.tsx';
+import App from './app';
 import './assets/globals.css';
 
 createRoot(document.getElementById('root')!).render(
