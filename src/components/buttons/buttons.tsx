622054a2c83729ef4ceb90f83f23a314d034914f
zacharie <zacharie@tshoko.fr>
2025-01-03 00:07:03 +0100

    add focus state on button

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -10,8 +10,8 @@ const Button = (props: ButtonProps) => {
     <button
       className={`flex justify-center items-center gap-2 rounded-md px-3 py-2 text-sm w-full ${
         props.variant === 'primary'
-          ? 'bg-foreground-accent text-white hover:bg-foreground-accentHover'
-          : 'bg-white hover:bg-gray-100 text-foreground-primary'
+          ? 'bg-foreground-accent text-white hover:bg-foreground-accentHover focus:ring-2 focus:ring-foreground-accentHover focus:ring-offset-1'
+          : 'bg-white hover:bg-gray-100 text-foreground-primary focus:ring-2 focus:ring-gray-200 focus:ring-offset-1'
       }`}
       onClick={props.onClick}
     >
