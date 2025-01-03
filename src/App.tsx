cb8ca7081ac0645770fd860230558d24559f2c34
zacharie <zacharie@tshoko.fr>
2025-01-03 19:16:28 +0100

    add toast

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ArtistsCard from './components/artists-card';
 import PlaygroundCard from './components/playground-card';
 import PreviewCard from './components/preview-card.png';
 import GithubIcon from './assets/icons/github-icon';
+import toast, { Toaster } from 'react-hot-toast';
 
 const m8_1Banner = new URL('assets/images/artists/m8/m8_1.png', import.meta.url)
   .href;
@@ -54,66 +55,73 @@ const App = () => {
         [blob.type]: blob,
       }),
     ]);
+
+    toast.success('Bannière copiée dans le presse-papiers', {
+      position: 'bottom-center',
+    });
   };
 
   return (
-    <div className="h-screen flex flex-col">
-      <header className="flex justify-center w-full font-cal mt-14">
-        <h1 className="text-foreground-primary text-center text-4xl font-bold leading-10">
-          Créer une bannière{' '}
-          <span className="whitespace-nowrap">
-            <M8Logo />
-            Gentle Mates,
-          </span>
-          <br />
-          <span className="text-foreground-accent">en quelques clics</span>
-        </h1>
-      </header>
+    <>
+      <div className="h-screen flex flex-col">
+        <header className="flex justify-center w-full font-cal mt-14">
+          <h1 className="text-foreground-primary text-center text-4xl font-bold leading-10">
+            Créer une bannière{' '}
+            <span className="whitespace-nowrap">
+              <M8Logo />
+              Gentle Mates,
+            </span>
+            <br />
+            <span className="text-foreground-accent">en quelques clics</span>
+          </h1>
+        </header>
 
-      <main className="flex flex-col xl:flex-row gap-6 xl:gap-4 m-auto py-10 px-4 w-screen max-w-[1440px] xl:h-[700px]">
-        <ArtistsCard
-          selectedBanner={selectedBanner}
-          onBannerChange={setSelectedBanner}
-        />
-        <PreviewCard
-          username={username}
-          role={role}
-          selectedBanner={
-            selectedBanner.bannerNumber === 1 ? m8_1Banner : m8_2Banner
-          }
-          onDownload={handleDownload}
-          onCopy={handleCopy}
-        />
-        <PlaygroundCard
-          username={username}
-          role={role}
-          onUsernameChange={(newUsername) =>
-            setUsername(newUsername.toUpperCase())
-          }
-          onRoleChange={(newRole) => setRole(newRole.toUpperCase())}
-        />
-      </main>
+        <main className="flex flex-col xl:flex-row gap-6 xl:gap-4 m-auto py-10 px-4 w-screen max-w-[1440px] xl:h-[700px]">
+          <ArtistsCard
+            selectedBanner={selectedBanner}
+            onBannerChange={setSelectedBanner}
+          />
+          <PreviewCard
+            username={username}
+            role={role}
+            selectedBanner={
+              selectedBanner.bannerNumber === 1 ? m8_1Banner : m8_2Banner
+            }
+            onDownload={handleDownload}
+            onCopy={handleCopy}
+          />
+          <PlaygroundCard
+            username={username}
+            role={role}
+            onUsernameChange={(newUsername) =>
+              setUsername(newUsername.toUpperCase())
+            }
+            onRoleChange={(newRole) => setRole(newRole.toUpperCase())}
+          />
+        </main>
 
-      <footer className="p-4 text-center text-foreground-secondary font-figtree text-sm">
-        <a
-          href="https://github.com/zachdlz/m8-banner-editor"
-          className="flex justify-center mb-1"
-        >
-          <GithubIcon />
-        </a>
-        <p>
-          Outil 100% gratuit. Développé par{' '}
-          <a href="https://x.com/ZzAK_K" className="font-semibold">
-            @ZzAK_K
+        <footer className="p-4 text-center text-foreground-secondary font-figtree text-sm">
+          <a
+            href="https://github.com/zachdlz/m8-banner-editor"
+            className="flex justify-center mb-1"
+          >
+            <GithubIcon />
           </a>
-          , designé par{' '}
-          <a href="https://x.com/reaiucas" className="font-semibold">
-            @reaiucas
-          </a>
-          .
-        </p>
-      </footer>
-    </div>
+          <p>
+            Outil 100% gratuit. Développé par{' '}
+            <a href="https://x.com/ZzAK_K" className="font-semibold">
+              @ZzAK_K
+            </a>
+            , designé par{' '}
+            <a href="https://x.com/reaiucas" className="font-semibold">
+              @reaiucas
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
+      <Toaster />
+    </>
   );
 };
 
