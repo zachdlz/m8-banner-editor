989cf6b95d5212a99e240a8feec0ac40f7362004
zacharie <zacharie@tshoko.fr>
2025-01-09 23:39:52 +0100

    Refactored the ArtistsCard component to improve the rendering of artist images, utilizing a memoized ArtistImage component for better performance.

--- src/components/artists-card/artists-card.tsx
+++ src/components/artists-card/artists-card.tsx
@@ -1,10 +1,35 @@
-import { Artist } from '../../utils/types';
+import { Artist, ArtistGroup } from '../../utils/types';
 import { ARTISTS } from '../../utils/artists';
+import { memo } from 'react';
 
 type ArtistsCardProps = {
   onArtistChange: (artist: Artist) => void;
   selectedArtist: Artist;
 };
+
+type ArtistImageProps = {
+  cover: ArtistGroup['covers'][number];
+  artistGroup: string;
+  onSelect: () => void;
+  isSelected: boolean;
+};
+
+const ArtistImage = memo(
+  ({ cover, artistGroup, onSelect, isSelected }: ArtistImageProps) => (
+    <div
+      className={`rounded-xl p-[2px] border-2 ${
+        isSelected ? 'border-foreground-accent' : 'border-transparent'
+      }`}
+    >
+      <img
+        src={cover.icon}
+        alt={artistGroup}
+        className="w-12 h-[46px] cursor-pointer rounded-lg"
+        onClick={() => onSelect()}
+      />
+    </div>
+  ),
+);
 
 const ArtistsCard = (props: ArtistsCardProps) => {
   return (
@@ -20,27 +45,21 @@ const ArtistsCard = (props: ArtistsCardProps) => {
             </h3>
             <div className="flex items-center gap-2">
               {artistGroup.covers.map((cover) => (
-                <div
+                <ArtistImage
                   key={`${artistGroup.group}-${cover.number}`}
-                  className={`rounded-xl p-[2px] border-2 ${
+                  cover={cover}
+                  artistGroup={artistGroup.group}
+                  onSelect={() =>
+                    props.onArtistChange({
+                      name: artistGroup.group,
+                      bannerNumber: cover.number,
+                    })
+                  }
+                  isSelected={
                     props.selectedArtist?.name === artistGroup.group &&
                     props.selectedArtist?.bannerNumber === cover.number
-                      ? 'border-foreground-accent'
-                      : 'border-transparent'
-                  }`}
-                >
-                  <img
-                    src={cover.icon}
-                    alt={artistGroup.group}
-                    className="w-12 h-[46px] cursor-pointer rounded-lg"
-                    onClick={() =>
-                      props.onArtistChange({
-                        name: artistGroup.group,
-                        bannerNumber: cover.number,
-                      })
-                    }
-                  />
-                </div>
+                  }
+                />
               ))}
             </div>
           </div>
