8527f4926a572111e90dda2aa746a7afc0630620
zacharie <zacharie@tshoko.fr>
2025-01-10 13:51:45 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './assets/styles/globals.css';
-import { inject } from '@vercel/analytics';
-
-inject();
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
